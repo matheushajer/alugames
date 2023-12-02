21182a875b8c98568aade83002ba494a1ab9cc7b
matheushajer <maatheushajer@gmail.com>
2023-12-02 11:52:18 -0300

    Criação da classe jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,15 @@
+class Jogo {
+
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+
+    override fun toString(): String {
+        return "Dados do Jogo: \n \n" +
+                "Titulo: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,25 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse.BodyHandlers
 
+const val URL_API_GAMES = "https://www.cheapshark.com/api/1.0/games?id="
+
 fun main() {
 
     val client: HttpClient = HttpClient.newHttpClient()
     val request = HttpRequest.newBuilder()
-        .uri(URI.create("https://www.cheapshark.com/api/1.0/games?id=612")).build()
+        .uri(URI.create(URL_API_GAMES + 612)).build()
 
     val response = client.send(request, BodyHandlers.ofString())
 
     val json = response.body()
 
     println(json)
+
+    val meuJogo = Jogo()
+    meuJogo.titulo = "jogo teste"
+    meuJogo.capa = "url teste"
+
+    println(meuJogo)
 
 
 }
